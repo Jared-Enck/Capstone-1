86d89756cda665d9ef5fc680160a4bff2ebda744
Jared Enck <jaredenck86@gmail.com>
2022-11-21 12:00:17 -0500

    add user model

--- models.py
+++ models.py
@@ -5,3 +5,23 @@ db = SQLAlchemy()
 def connect_db(app):
     db.app = app
     db.init_app(app)
+    
+class User(db.Model):
+    """User."""
+    
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, 
+                   primary_key=True, 
+                   autoincrement=True)
+    
+    username = db.Column(db.String(20), 
+                         nullable=False, 
+                         unique=True)
+    
+    password = db.Column(db.Text, 
+                         nullable=False)
+    
+    email = db.Column(db.Text, 
+                      nullable=False, 
+                      unique=True)
