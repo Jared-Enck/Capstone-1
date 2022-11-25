dcb2106f31af91dc2a6f565d2e67166acfd6d5fd
Jared Enck <jaredenck86@gmail.com>
2022-11-25 14:51:56 -0500

    updated forms to flask-wtf

--- forms.py
+++ forms.py
@@ -1,22 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms_alchemy import model_form_factory
-from wtforms import PasswordField, EmailField
-from wtforms.validators import DataRequired, Email, Length, Optional
+from wtforms import PasswordField, StringField, EmailField
+from wtforms.validators import Length, Optional, DataRequired, Email, URL
 from models import db, User, Deck, Comment
-
-BaseModelForm = model_form_factory(FlaskForm)
-
-class ModelForm(BaseModelForm):
-    @classmethod
-    def get_session(self):
-        return db.session
     
-class RegisterForm(ModelForm):
-    class Meta:
-        model = User
-        password = PasswordField(validators=[Length(min=6)])
+class RegisterForm(FlaskForm):
+    """Form for adding users."""
+
+    username = StringField('Username', validators=[DataRequired(message='Enter a username.')])
+    password = PasswordField('Password', validators=[Length(min=6, message='Password be at least 6 characters.')])
+    email = EmailField('Email', validators=[Email(message='Enter a valid email.')])
         
-class LoginForm(ModelForm):
-    class Meta:
-        model = User
-        only = ['username', 'password']
+class LoginForm(FlaskForm):
+    """Log in user form"""
+    
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[Length(min=6)])
