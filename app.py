4dcc4998178bd8fc1107270960c7fbbea10dade4
Jared Enck <jaredenck86@gmail.com>
2023-01-05 18:20:17 -0500

    add adv search and liked decks pages with routes

--- app.py
+++ app.py
@@ -2,8 +2,9 @@ from flask_cors import CORS
 from flask import Flask, redirect, render_template, request, flash, jsonify, url_for
 from flask_login import LoginManager, login_user, login_required, logout_user, current_user
 from flask_debugtoolbar import DebugToolbarExtension
-from models import db, connect_db, User, Card, MainDecklist, EggDecklist, SideDecklist, Deck, SharedDeck, MainDeckCard, EggDeckCard, SideDeckCard
+from models import db, connect_db, User, Card, MainDecklist, EggDecklist, SideDecklist, Deck, SharedDeck, MainDeckCard, EggDeckCard, SideDeckCard, DeckLikes
 from forms import RegisterForm, LoginForm, EditUserForm,AdvancedSearchForm
+from sqlalchemy.exc import IntegrityError
 
 app = Flask(__name__)
 CORS(app)
@@ -33,21 +34,18 @@ def unauthorized():
 
 login_manager.login_view = 'login'
 
-app.jinja_env.globals.update(main_cards=MainDecklist.main_cards)
-app.jinja_env.globals.update(egg_cards=EggDecklist.egg_cards)
-app.jinja_env.globals.update(side_cards=SideDecklist.side_cards)
-app.jinja_env.globals.update(highest_dp_card_img=MainDecklist.highest_dp_card_img)
+app.jinja_env.globals.update(get_stats=Card.serialize_stats)
 
 @app.route('/', methods=['GET'])
 def homepage():
     """Show homepage."""
-    
+        
     shared_decks = SharedDeck.query.order_by(SharedDeck.timestamp.desc()).limit(20).all()
     
     return render_template('index.html', shared_decks=shared_decks)
 
 ############################
-######## User views ########
+######## User routes ########
 
 @app.route('/register', methods=['GET','POST'])
 def register():
@@ -55,15 +53,22 @@ def register():
     
     form = RegisterForm()
     
-    if form.validate_on_submit():
-        user = User.register(form.data)
+    try:
+        if form.validate_on_submit():
+            user = User.register(form.data)
+            
+            login_user(user)
+            
+            flash('Successfully created your account!','success')
+            
+            return redirect(url_for('homepage'))
+    except IntegrityError as err:
+        err_info = jsonify(err.orig.args[0]).get_json()
         
-        login_user(user)
-        
-        flash('Successfully created your account!','success')
-        
-        return redirect(url_for('homepage'))
-        
+        if "users_username_key" in err_info:
+            flash('Username already taken.', 'danger')
+        if "users_email_key" in err_info:
+            flash('Email already taken.', 'danger')
     
     return render_template('/user/register.html', form=form)        
 
@@ -110,7 +115,7 @@ def user_details(user_id):
 def edit_user(user_id):
     """Update info for current user."""
     
-    user = User.query.get_or_404(user_id)    
+    user = User.query.get_or_404(user_id)   
     form = EditUserForm(obj=user)
     
     if form.validate_on_submit():
@@ -118,7 +123,7 @@ def edit_user(user_id):
         
         db.session.commit()
         
-        flash(f"{user.username}'s info has been updated.", 'success')
+        flash(f"{user.username}'s info has been updated.", 'info')
         
         return redirect(url_for('user_details', user_id=user_id))
     
@@ -138,9 +143,19 @@ def delete_user():
     flash('Deleted your account.', 'info')
     
     return redirect(url_for('homepage'))
+
+@app.route('/users/<int:user_id>/likes')
+def user_deck_likes(user_id):
+    """Gets all decks that a user liked."""
+    
+    user = User.query.get_or_404(user_id)
+    
+    liked_decks = db.session.query(SharedDeck).join(DeckLikes).filter(DeckLikes.user_id == user.id).all()
+    
+    return render_template('/user/liked_decks.html', user=user, liked_decks=liked_decks)
     
 ############################
-######## Card views ########
+######## Card routes ########
 
 @app.route('/cards/<number>', methods=['GET','PATCH'])
 def show_card(number):
@@ -162,9 +177,17 @@ def show_adv_search():
     
     return render_template('/search/adv_search.html')
 
+@app.route('/cards/advanced')
+def adv_search():
+    """Show adv search html."""
+    
+    adv_form = AdvancedSearchForm()
+    
+    return render_template('/card/adv_search.html',adv_form=adv_form)
+
 
 ############################
-######## Deck views ########
+######## Deck routes ########
 
 @app.route('/decks', methods=['GET','POST'])
 @login_required
@@ -207,8 +230,8 @@ def user_decks():
 def show_deck(deck_id):
     """Shows deck, or allows deck owner to share/delete."""
     
-    deck = Deck.query.get_or_404(deck_id)
-    
+    deck = Deck.query.get_or_404(deck_id) 
+
     if request.method == 'POST':
         
         db.session.delete(deck)
@@ -222,7 +245,20 @@ def show_deck(deck_id):
         'Side': SideDecklist.side_cards(deck.side_decklist_id)
     }
     
-    return render_template('/deck/deck_details.html',deck=deck,decklist=decklist)
+    def list_str_stats(stat_obj): 
+        
+        return [ stat + ': ' + stat_obj[stat] for stat in stat_obj if stat_obj[stat] != None and stat != 'image_url' ]
+
+    if current_user.is_authenticated:
+        user = User.query.get_or_404(current_user.id)
+    
+        liked_decks = db.session.query(SharedDeck).join(DeckLikes).filter(DeckLikes.user_id == user.id).all()
+        
+        liked_deck_ids = [deck.id for deck in liked_decks]
+        
+        return render_template('/deck/deck_details.html',deck=deck,decklist=decklist, list_str_stats=list_str_stats,likes=liked_deck_ids)
+    else:
+        return render_template('/deck/deck_details.html',deck=deck,decklist=decklist, list_str_stats=list_str_stats)
 
 @app.route('/share/<int:deck_id>', methods=['POST'])
 @login_required
@@ -242,3 +278,29 @@ def share_deck(deck_id):
     flash(f'Shared new deck {deck.name}', 'success')
     
     return redirect(url_for('homepage'))
+
+@app.route('/decks/<int:deck_id>/likes', methods=['POST'])
+@login_required
+def liked_deck(deck_id):
+    """Toggle deck like for logged in user and add or remove like from DeckLikes."""
+    
+    user = User.query.get_or_404(current_user.id)
+    
+    liked_deck = SharedDeck.query.filter(SharedDeck.deck_id == deck_id).first()
+    
+    user_likes = db.session.query(SharedDeck).join(DeckLikes).filter(DeckLikes.user_id == user.id).all()
+    
+    if liked_deck in user_likes:
+        user_likes = [like for like in user_likes if like != liked_deck]
+        like = DeckLikes.query.filter(DeckLikes.shared_deck_id == liked_deck.id).first()
+        db.session.delete(like)
+    else:
+        user_likes.append(liked_deck)
+        new_like = DeckLikes(shared_deck_id=liked_deck.id, user_id=user.id)
+        
+        db.session.add(new_like)
+    
+    db.session.commit()
+    
+    return redirect( url_for('show_deck',deck_id=deck_id) )
+
