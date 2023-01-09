c0243e4f8fe1f701ea3e098818e9569f8e047587
Jared Enck <jaredenck86@gmail.com>
2023-01-09 16:32:52 -0500

    mv secret to gitignore

---
+++ main/config.py
@@ -0,0 +1,12 @@
+import os
+from instance.secrets import SECRET_KEY
+
+SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql:///DCG_db').replace('://', 'ql://', 1)
+
+SECRET_KEY_VAL = os.environ.get('SECRET_KEY', SECRET_KEY)
+
+DEBUG = False
+
+SQLALCHEMY_ECHO = False
+
+DEBUG_TB_INTERCEPT_REDIRECTS = True
