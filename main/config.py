c8adc84fc63c0cfdec24f915f8a29fe0592394a4
Jared Enck <jaredenck86@gmail.com>
2023-01-09 18:50:13 -0500

    ...

--- main/config.py
+++ main/config.py
@@ -1,9 +1,9 @@
-import os     
-from instance.secret_keys import SECRET_KEY
+import os 
+# from .instance.secret_keys import SECRET_KEY
 
 SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql:///DCG_db').replace('://', 'ql://', 1)
 
-SECRET_KEY_VAL = os.environ.get('SECRET_KEY', SECRET_KEY)
+SECRET_KEY_VAL = os.environ.get('SECRET_KEY', 'super_secret_key')
 
 DEBUG = False
 
