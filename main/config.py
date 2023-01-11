1c7b8d99f624ee1cb3c558bdc5eff9ac21a9042a
Jared Enck <jaredenck86@gmail.com>
2023-01-11 13:58:57 -0500

    config update

--- main/config.py
+++ main/config.py
@@ -2,9 +2,14 @@ import os
 try:
     from .instance.secret_keys import SECRET_KEY
 except:
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'super_secret_key')
+    SECRET_KEY = os.environ.get('SECRET_KEY', None)
+    
+SECRET_KEY = os.environ.get('SECRET_KEY', SECRET_KEY)
 
-SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql:///DCG_db').replace('://', 'ql://', 1)
+try:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql:///DCG_db')
+except:
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'postgresql:///DCG_db').replace('://', 'ql://', 1)
 
 DEBUG = False
 
@@ -12,3 +17,4 @@ if DEBUG:
     SQLALCHEMY_ECHO = True
 
     DEBUG_TB_INTERCEPT_REDIRECTS = True
+    
